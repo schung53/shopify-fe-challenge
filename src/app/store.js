a7420a4a3bb335826dfede4f100351b98d9e5694
James Chung <sunhochung53@gmail.com>
2022-01-08 11:10:18 -0800

    Delete boilerplate Redux code

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import datesReducer from '../features/dates/datesSlice';
 import picturesReducer from '../features/pictures/picturesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     dates: datesReducer,
     pictures: picturesReducer
   }
